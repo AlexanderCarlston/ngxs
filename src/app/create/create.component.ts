60e23448ce50869ad15daa650a6b43432f117a13
alekscarlston@yahoo.com <alekscarlston@yahoo.com>
2019-01-19 16:35:22 -0700

    updated app.module

--- src/app/create/create.component.ts
+++ src/app/create/create.component.ts
@@ -1,13 +1,16 @@
 import { Component, OnInit } from '@angular/core';
+import { Store } from '@ngxs/store';
+import { AddTutorial } from './../actions/tutorial.actions'
 
 @Component({
   selector: 'app-create',
   templateUrl: './create.component.html',
   styleUrls: ['./create.component.css']
 })
+
 export class CreateComponent implements OnInit {
 
-  constructor() { }
+  constructor(private store: Store) { }
 
   ngOnInit() {
   }
